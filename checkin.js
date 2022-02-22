b2ac32d181fa53bdaefd3df22f2d3733da6b8734
dullsword <525649253@qq.com>
2022-02-23 04:48:24 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+import axios from "axios";
 
 const SCKEY = process.env.SCKEY;
 
@@ -13,7 +13,7 @@ const checkIn = async (cookie) => {
             token: "glados_network"
         }
     })
-}
+};
 
 const getStatus = async (cookie) => {
     return axios({
@@ -23,7 +23,7 @@ const getStatus = async (cookie) => {
             'Cookie': cookie
         }
     })
-}
+};
 
 const checkInAndGetStatus = async (cookie) => {
     const checkInMessage = (await checkIn(cookie))?.data?.message;
@@ -32,33 +32,40 @@ const checkInAndGetStatus = async (cookie) => {
     const email = userStatus?.email;
     const leftDays = parseInt(userStatus?.leftDays);
 
-    console.log(checkInMessage, email, leftDays);
     return {
         checkInMessage,
         email,
         leftDays
     };
-}
+};
 
-// const push = (checkInMessage, email, leftDays) => {
-//     return axios({
-//         method: 'get',
-//         url: `https://sctapi.ftqq.com/${SCKEY}.send`,
-//         params: {
-//             title: `${leftDays}天后到期，${checkInMessage}`
-//         }
-//     })
-// }
+const push = (infos) => {
+    let desp = '';
+    for (const info of infos) {
+        const { checkInMessage, email, leftDays } = info;
+
+        desp += `${email}：${leftDays}，${checkInMessage}\n`;
+    }
+
+    return axios({
+        method: 'post',
+        url: `https://sctapi.ftqq.com/${SCKEY}.send`,
+        data: {
+            title: `${infos?.[0].email}：${infos?.[0].leftDays}，${infos?.[0].checkInMessage}`,
+            desp
+        }
+    })
+};
 
 const GLaDOSCheckIn = async () => {
     const cookies = process.env.COOKIES?.split(",") ?? [];
 
-    const result = await Promise.all(cookies.map(async cookie => await checkInAndGetStatus(cookie)));
+    const results = await Promise.all(cookies.map(async cookie => await checkInAndGetStatus(cookie)));
+    console.log(results);
 
-    console.log(result);
-    // if (SCKEY) {
-    //     server(checkInMessage, email, leftDays);
-    // }
-}
+    if (SCKEY) {
+        push(results);
+    }
+};
 
 GLaDOSCheckIn();
