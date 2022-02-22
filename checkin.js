5cd59639d2c132a8ce714ab9c56b8e2a164a3c94
dullsword <525649253@qq.com>
2022-02-23 04:01:01 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -1,60 +1,61 @@
 const axios = require("axios");
 
 const SCKEY = process.env.SCKEY;
-axios.defaults.headers.common.cookie = process.env.COOKIE;
 
 const checkIn = async (cookie) => {
     return axios({
         method: 'post',
         url: 'https://glados.rocks/api/user/checkin',
+        headers: {
+            'Cookie': cookie
+        },
         data: {
             token: "glados_network"
         }
     })
 }
 
-const status = async (cookie) => {
+const getStatus = async (cookie) => {
     return axios({
         method: 'get',
-        url: 'https://glados.rocks/api/user/status'
-    })
-}
-
-const push = (checkInMessage, email, leftDays) => {
-    return axios({
-        method: 'get',
-        url: `https://sctapi.ftqq.com/${SCKEY}.send`,
-        params: {
-            title: `${leftDays}天后到期，${checkInMessage}`
+        url: 'https://glados.rocks/api/user/status',
+        headers: {
+            'Cookie': cookie
         }
     })
 }
 
-const executeCheckIn = async () => {
-    const cookies = process.env.COOKIES?.split(",") ?? [];
-    console.log(cookies);
+const checkInAndGetStatus = async (cookie) => {
+    const checkInMessage = (await checkIn(cookie))?.data?.message;
 
-    // const result = [];
+    const userStatus = (await getStatus(cookie))?.data?.data;
+    const email = userStatus?.email;
+    const leftDays = parseInt(userStatus?.leftDays);
 
-    // for (const cookie of cookies) {
-    //     const checkInMessage = (await checkIn(cookie))?.data?.message;
-
-    //     const userStatus = (await status(cookie))?.data?.data;
-    //     const email = userStatus?.email;
-    //     const leftDays = parseInt(userStatus?.leftDays);
-
-    //     console.log(checkInMessage, email, leftDays);
-    //     result.push({
-    //         checkInMessage,
-    //         email,
-    //         leftDays
-    //     })
-    // }
-    // return result;
+    console.log(checkInMessage, email, leftDays);
+    return {
+        checkInMessage,
+        email,
+        leftDays
+    };
 }
 
+// const push = (checkInMessage, email, leftDays) => {
+//     return axios({
+//         method: 'get',
+//         url: `https://sctapi.ftqq.com/${SCKEY}.send`,
+//         params: {
+//             title: `${leftDays}天后到期，${checkInMessage}`
+//         }
+//     })
+// }
+
 const GLaDOSCheckIn = async () => {
-    await executeCheckIn();
+    const cookies = process.env.COOKIES?.split(",") ?? [];
+
+    const result = await Promise.all(cookies.map(async cookie => await checkInAndGetStatus(cookie)));
+
+    console.log(result);
     // if (SCKEY) {
     //     server(checkInMessage, email, leftDays);
     // }
