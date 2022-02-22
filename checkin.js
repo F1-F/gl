fa3d46eb4201a9b2930f7b9d7b28237c1ca06cdd
dullsword <525649253@qq.com>
2022-02-22 21:35:04 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const SCKEY = process.env.SCKEY;
 axios.defaults.headers.common.cookie = process.env.COOKIE;
 
-const checkIn = async () => {
+const checkIn = async (cookie) => {
     return axios({
         method: 'post',
         url: 'https://glados.rocks/api/user/checkin',
@@ -13,14 +13,14 @@ const checkIn = async () => {
     })
 }
 
-const status = async () => {
+const status = async (cookie) => {
     return axios({
         method: 'get',
         url: 'https://glados.rocks/api/user/status'
     })
 }
 
-const server = (checkInMessage, leftDays) => {
+const push = (checkInMessage, email, leftDays) => {
     return axios({
         method: 'get',
         url: `https://sctapi.ftqq.com/${SCKEY}.send`,
@@ -30,13 +30,33 @@ const server = (checkInMessage, leftDays) => {
     })
 }
 
+const executeCheckIn = async () => {
+    const cookies = process.env.COOKIES;
+    console.log(typeof cookies);
+    // const result = [];
+
+    // for (const cookie of cookies) {
+    //     const checkInMessage = (await checkIn(cookie))?.data?.message;
+
+    //     const userStatus = (await status(cookie))?.data?.data;
+    //     const email = userStatus?.email;
+    //     const leftDays = parseInt(userStatus?.leftDays);
+
+    //     console.log(checkInMessage, email, leftDays);
+    //     result.push({
+    //         checkInMessage,
+    //         email,
+    //         leftDays
+    //     })
+    // }
+    // return result;
+}
+
 const GLaDOSCheckIn = async () => {
-    const checkInMessage = (await checkIn())?.data?.message;
-    const leftDays = parseInt((await status())?.data?.data?.leftDays);
-    console.log(leftDays, checkInMessage);
-    if (SCKEY) {
-        server(checkInMessage, leftDays);
-    }
+    await executeCheckIn();
+    // if (SCKEY) {
+    //     server(checkInMessage, email, leftDays);
+    // }
 }
 
 GLaDOSCheckIn();
